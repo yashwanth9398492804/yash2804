8aa81cdf153a7a0f9c8a7129d53b8b1d773208da
yashwanth <yashwanthkanaparthi@gmail.com>
2021-02-19 11:23:10 +0530

    about.java modified

--- about.java
+++ about.java
@@ -1,7 +1,8 @@
-class about
+class About
 {
 	public static void main(String args[])
 	{
-		System.out,println("chaliye shuru karthe hei");
+		System.out.println("Hello ji");
+		System.out.println("chaliye shuru karthe hei");
 	}
 }
